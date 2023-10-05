ec07f5d9ae69e929fe8fae8fd5a441f6f266750a
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-05 17:57:22 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        //console.log(event.target.children[1].value)
+        //or
+        
+        const input = document.querySelector('input#searchByID');
+        console.log(input.value)  
+        
+        //search for our movie by id
+       fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        }) 
+
+        //display our movie on the page 
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data =>{
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+    })
   
 }
 
